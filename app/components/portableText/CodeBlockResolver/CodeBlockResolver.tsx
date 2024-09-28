6ae76e513e52c0d0ab3590866516cb15e3688be8
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2024-09-29 01:23:45 +0200

    Add markers css class (#2)
    ## Background
    
    We want to be able to hightlight lines in the code block, but this is
    not supported out of the box and requires custom styling.
    
    ## Solution
    
    Move the CodeBlockResolver to its own folder, and create a css file that
    holds the styling for the `refractor-marker` css class. This class is
    used to override the syntax highlighting. As this file is relevant to
    show code examples of, it also makes sense to register `css` as a
    language from refractor in the process.

--- app/components/portableText/CodeBlockResolver/CodeBlockResolver.tsx
+++ app/components/portableText/CodeBlockResolver/CodeBlockResolver.tsx
@@ -1,12 +1,15 @@
 import 'prismjs/themes/prism.css';
 import { Refractor, registerLanguage } from "react-refractor";
+import css from "refractor/lang/css";
 import ts from "refractor/lang/typescript";
-import { CodeBlock } from "../../sanity/types";
+import { CodeBlock } from "../../../sanity/types";
+import './codeBlockResolver.css';
 
 type Props = {
   value: CodeBlock;
 }
 
+registerLanguage(css)
 registerLanguage(ts)
 
 export const CodeBlockResolver = ({ value }: Props) => {

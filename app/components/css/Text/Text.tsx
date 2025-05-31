817f9018bfa90eabb85ff4788f5acb83addd8229
Kristoffer Æsøy <47326965+KrisAesoey@users.noreply.github.com>
2025-05-31 19:12:05 +0200

    Add support for different text styles

--- app/components/css/Text/Text.tsx
+++ app/components/css/Text/Text.tsx
@@ -3,8 +3,9 @@ import './text.css'
 type Props = {
   children: string | React.ReactNode
   fontSize: 'sm' | 'md' | 'lg'
+  style?: 'primary' | 'secondary' | 'muted' | 'highlight'
 }
 
-export const Text = ({ children, fontSize }: Props) => (
-  <div className={`text text-${fontSize}`}>{children}</div>
+export const Text = ({ children, fontSize, style }: Props) => (
+  <div className={`text text-${fontSize} text-${style}`}>{children}</div>
 )
